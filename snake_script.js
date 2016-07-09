3f1247b9b0afee460ccb5c985c82656fa385520f
volartem <volartem7@gmail.com>
2016-07-09 14:48:22 +0300

    commit 3

--- snake_script.js
+++ snake_script.js
@@ -3,7 +3,7 @@ onload = function () {
     snake.createMatrix();
     snake.createSnake(5, 10, 5, "left");
     snake.randomInteger(0, 399);
-    document.body.addEventListener("keyup", snake.move);
+    document.body.addEventListener("keydown", snake.move);
     document.getElementById("pause").addEventListener("click", snake.pause);
     console.log("Lets start!!!");
 }
@@ -13,7 +13,7 @@ snake = {
     current_move: 0,
     snake_arr: [],
     count: 0,
-    
+
     pause: function () {
         if (snake.current_move != 0) {
             clearInterval(snake.current_move);
@@ -26,12 +26,12 @@ snake = {
         rand = Math.round(rand);
         snake.current_random = rand;
         for (var i = 0; i < snake.snake_arr.length; i++) {
-                var rrr = snake.getCell(snake.snake_arr[i].row, snake.snake_arr[i].col);
-                if (rrr === rand) {
-                    stop = false;
-                    snake.randomInteger(min, max);
-                } 
+            var rrr = snake.getCell(snake.snake_arr[i].row, snake.snake_arr[i].col);
+            if (rrr === rand) {
+                stop = false;
+                snake.randomInteger(min, max);
             }
+        }
         if (stop) {
             return matrix.children[rand].style.backgroundColor = "#ff6a00";
         }
@@ -46,7 +46,7 @@ snake = {
             div.className = 'cell';
             matrix.appendChild(div);
         }
-        
+
     },
 
     getCell: function (row, col) {
@@ -61,9 +61,9 @@ snake = {
             matr.style.backgroundColor = "transparent";
         }
     },
-    createSnake: function (row, col, count, course) { 
+    createSnake: function (row, col, count, course) {
         for (var i = 0; i < count; i++) {
-            if (course === "left" ) {
+            if (course === "left") {
                 snake.setCell(row, col + i, true);
                 snake.snake_arr.push({ row: row, col: col + i });
             } else if (course === "right") {
@@ -77,6 +77,7 @@ snake = {
                 snake.snake_arr.push({ row: row - i, col: col });
             }
         }
+        document.getElementById("matrix").children[snake.getCell(snake.snake_arr[0].row, snake.snake_arr[0].col)].style.backgroundColor = "#ff6a00";
     },
     snakeTrueCell: function (row, col) {
         for (var i = 0; i < snake.snake_arr.length; i++) {
@@ -88,11 +89,13 @@ snake = {
     snakeReset: function () {
         alert(snake.count);
         clearInterval(snake.current_move);
-        for (var i = snake.snake_arr.length-1; i >= 0; i--) {
+        for (var i = snake.snake_arr.length - 1; i >= 0; i--) {
             snake.setCell(snake.snake_arr[i].row, snake.snake_arr[i].col, false);
             snake.snake_arr.pop();
         }
-        snake.createSnake(10, 10, 5, "left");
+        document.getElementById("matrix").children[snake.current_random].style.backgroundColor = "transparent";
+        snake.createSnake(10, 10, 5, "right");
+        snake.randomInteger(0, 399);
         snake.count = 0;
     },
 
@@ -125,21 +128,17 @@ snake = {
                                 snake.snake_arr[i].col = temp.col;
                             }
                             if (snake.current_random === snake.getCell(snake.snake_arr[0].row, snake.snake_arr[0].col)) {
-                                //snake.setCell(tail_row, tail_col, true); // or delete this code
                                 snake.count++;
                                 snake.snake_arr.push({ row: tail_row, col: tail_col });
                                 snake.randomInteger(0, 399);
                             }
-                            //else {
-                            //    snake.setCell(tail_row, tail_col, false);
-                            //}
                         } else {
 
                             snake.snakeReset();
                             return;
                         }
                     } else {
-                        console.log("ELSE LEFT");
+                        console.log("LEFT");
                         snake.snakeReset();
                         return;
                     }
@@ -155,20 +154,16 @@ snake = {
                                 snake.snake_arr[i].col = temp.col;
                             }
                             if (snake.current_random === snake.getCell(snake.snake_arr[0].row, snake.snake_arr[0].col)) {
-                                //snake.setCell(tail_row, tail_col, true); // or delete this code
                                 snake.count++;
                                 snake.snake_arr.push({ row: tail_row, col: tail_col });
                                 snake.randomInteger(0, 399);
                             }
-                            //else {
-                            //    snake.setCell(tail_row, tail_col, false);
-                            //}
                         } else {
                             snake.snakeReset();
                             return;
                         }
                     } else {
-                        console.log("ELSE RIGHT");
+                        console.log("RIGHT");
                         snake.snakeReset();
                         return;
                     }
@@ -184,20 +179,16 @@ snake = {
                                 snake.snake_arr[i].col = temp.col;
                             }
                             if (snake.current_random === snake.getCell(snake.snake_arr[0].row, snake.snake_arr[0].col)) {
-                                //snake.setCell(tail_row, tail_col, true); // or delete this code
                                 snake.count++;
                                 snake.snake_arr.push({ row: tail_row, col: tail_col });
                                 snake.randomInteger(0, 399);
                             }
-                            //else {
-                            //    snake.setCell(tail_row, tail_col, false);
-                            //}
                         } else {
                             snake.snakeReset();
                             return;
                         }
                     } else {
-                        console.log("ELSE UP");
+                        console.log("UP");
                         snake.snakeReset();
                         return;
                     }
@@ -213,20 +204,16 @@ snake = {
                                 snake.snake_arr[i].col = temp.col;
                             }
                             if (snake.current_random === snake.getCell(snake.snake_arr[0].row, snake.snake_arr[0].col)) {
-                                //snake.setCell(tail_row, tail_col, true); // or delete this code
                                 snake.count++;
                                 snake.snake_arr.push({ row: tail_row, col: tail_col });
                                 snake.randomInteger(0, 399);
                             }
-                            //else {
-                            //    snake.setCell(tail_row, tail_col, false);
-                            //}
                         } else {
                             snake.snakeReset();
                             return;
                         }
                     } else {
-                        console.log("ELSE DOWN");
+                        console.log("DOWN");
                         snake.snakeReset();
                         return;
                     }
@@ -234,7 +221,9 @@ snake = {
                 snake.setCell(snake.snake_arr[i].row, snake.snake_arr[i].col, true);
             }
             snake.setCell(tail_row, tail_col, false);
-        }, 200);
+            document.getElementById("matrix").children[snake.getCell(snake.snake_arr[0].row, snake.snake_arr[0].col)].style.backgroundColor = "#ff6a00";
+            //snake.current_move = setTimeout(snake.current_move, 100);
+        }, 100);
     }
-    
+
 }
